c5822194adc074f6d3810c553d4f216cacae1ed9
肖东明 <xdm.hi@163.com>
2017-04-20 21:53:00 +0800

    add Cookie class

---
+++ src/System/Lib/Cookie.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmxiao
+ * Date: 17/4/20
+ * Time: 下午9:46
+ */
+
+namespace App\System\Lib;
+
+
+class Cookie
+{
+    private $prefix;
+
+    public function __construct()
+    {
+    }
+
+
+    /**
+     * @param $name
+     * @param $default
+     */
+    public function get($name, $default){
+
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     * @param $prefix
+     * @param $expire
+     * @param $domain
+     * @param $path
+     */
+    public function set($name, $value, $prefix, $expire, $domain, $path){
+
+    }
+
+    /**
+     * @param $name
+     */
+    public function del($name){
+
+    }
+
+}
