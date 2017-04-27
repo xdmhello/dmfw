c276f38fd44a874c0efeef4cd7d6aba3701dda13
xdmhello <xdm.hi@163.com>
2017-04-27 11:09:37 +0800

    添加缓存机制

---
+++ src/System/Lib/Redis.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: dmxiao@enet.com.cn
+ * Date: 2017/4/27
+ * Time: 10:17
+ * Filename: Redis.php
+ */
+
+namespace App\System\Lib;
+
+
+/**
+ * Class Redis
+ * @package App\System\Lib
+ */
+class Redis
+{
+
+    /**
+     * Redis constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    function __call($name, $arguments)
+    {
+
+    }
+
+
+
+}
